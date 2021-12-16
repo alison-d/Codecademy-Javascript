0c9a8573997dabd45f2487ded73155c70505af17
alison-d <adaurio@gmail.com>
2021-12-16 11:02:51 -0700

    Dog Years Codecademy project
    Added second Codecademy project

---
+++ Dog-Years.js
@@ -0,0 +1,22 @@
+const myAge = 38;
+//my age
+
+let earlyYears = 2;
+// early years
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+// later years
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+myAgeInDogYears = earlyYears + laterYears;
+//my age in dog years
+
+const myName = 'Alison'.toLowerCase();
+//my name
+
+console.log(`My name is ${myName}. I am ${myAge} in human years which is ${myAgeInDogYears} years old in dog years.`);
+//my age converted to dog years
